87b455ef28fbfecb101044d9095fdfc8276185be
TJ Horner <rowenrocks@gmail.com>
2014-08-19 20:58:53 -0700

    first commit

---
+++ src/net/iquestria/java/iQuestria.java
@@ -0,0 +1,13 @@
+package net.iquestria.java;
+
+import net.iquestria.java.net.iquestria.java.models.User;
+
+/**
+ * (c) iQuestria Team 2014
+ * See LICENSE file for license details.
+ */
+public class iQuestria {
+    public static User getUserById(Integer id){
+        return new User();
+    }
+}
---
+++ src/net/iquestria/java/net/iquestria/java/models/User.java
@@ -0,0 +1,14 @@
+package net.iquestria.java.net.iquestria.java.models;
+
+/**
+ * (c) iQuestria Team 2014
+ * See LICENSE file for license details.
+ */
+public class User {
+    private Integer id;
+    private String username;
+    private String realName;
+    private String bio;
+    private Rank rank;
+    private String email;
+}
